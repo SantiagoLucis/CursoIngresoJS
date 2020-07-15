8b85dcf48c5444ee38fc7887fd6ab482efa30884
unknown <santiagolucis9@gmail.com>
2020-07-15 18:56:12 -0300

    tp n3 resuelto

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -11,13 +11,13 @@ function sumar()
 	var numero2;
 	var resultado;
 
-	numero1=txtIdNumeroUno.value;
-	numero1=parseInt(numero1);
+	numero1= txtIdNumeroUno.value;
+	numero1= parseInt(numero1);
 
-	numero2=txtIdNumeroDos.value;
-	numero2=parseInt(numero2);
+	numero2= txtIdNumeroDos.value;
+	numero2= parseInt(numero2);
 
-	resultado=numero1+numero2;
+	resultado= numero1 + numero2;
 
 	alert(resultado);
 
@@ -26,13 +26,13 @@ function sumar()
 
 function restar()
 {
-	var numero1=txtIdNumeroUno.value;
-	var numero1=parseInt(numero1);
+	var numero1= txtIdNumeroUno.value;
+	var numero1= parseInt(numero1);
 
-	var numero2=txtIdNumeroDos.value;
-	var numero2=parseInt(numero2);
+	var numero2= txtIdNumeroDos.value;
+	var numero2= parseInt(numero2);
 
-	resultado=numero1 - numero2;
+	resultado= numero1 - numero2;
 
 	alert(resultado);
 
@@ -40,13 +40,13 @@ function restar()
 
 function multiplicar()
 { 
-	var numero1=txtIdNumeroUno.value;
-	var numero1=parseInt(numero1);
+	var numero1= txtIdNumeroUno.value;
+	var numero1= parseInt(numero1);
 
-	var numero2=txtIdNumeroDos.value;
-	var numero2=parseInt(numero2);
+	var numero2= txtIdNumeroDos.value;
+	var numero2= parseInt(numero2);
 
-	resultado=numero1*numero2;
+	resultado=numero1 * numero2;
 
 	alert(resultado);
 	
@@ -55,14 +55,15 @@ function multiplicar()
 
 function dividir()
 {
-	var numero1=txtIdNumeroUno.value;
-	var numero1=parseInt(numero1);
+	var numero1= txtIdNumeroUno.value;
+	var numero1= parseInt(numero1);
 
-	var numero2=txtIdNumeroDos.value;
-	var numero2=parseInt(numero2);
+	var numero2= txtIdNumeroDos.value;
+	var numero2= parseInt(numero2);
 
-	resultado=numero1/numero2;
+	resultado= numero1 / numero2;
 
 	alert(resultado);
+	}
 
-}
+
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,40 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
+	var fahrenheit;
+	var centígrados;
+	var resultadoFC;
+
+	var fahrenheit=txtIdTemperatura.value;
+	var centígrados=txtIdTemperatura.value;
+
+	var fahrenheit=parseInt(fahrenheit);
+	var centígrados=parseInt(centígrados);
+
+	resultadoFC=(centígrados-32)/1.8;
+
+
+	alert(+fahrenheit+ " Fahrenheit son " +resultadoFC+ " Centígrados");
+
+
 	
 }
 
 function CentigradosFahrenheit () 
 {
+	var fahrenheit;
+	var centígrados;
+	var resultadoCF;
+
+	var fahrenheit=txtIdTemperatura.value;
+	var centígrados=txtIdTemperatura.value;
+
+	var fahrenheit=parseInt(fahrenheit);
+	var centígrados=parseInt(centígrados);
+
+	resultadoCF=fahrenheit*1.8+32;
+
+	alert(+centígrados+ " Centígrados son " +resultadoCF+ " Fahrenheit");
 	
+
 }
